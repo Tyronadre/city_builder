b04188e3ab21fc4553054c4692ec2f662f41c1b2
Tyronadre <crafter.birne@gmx.de>
2022-10-31 19:21:22 +0100

    stuff

--- src/engine/cards/CardBuilder.java
+++ src/engine/cards/CardBuilder.java
@@ -3,8 +3,15 @@ package engine.cards;
 import eea.engine.event.Event;
 import engine.interfaces.IGameBoard;
 import engine.interfaces.IPlayer;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CardBuilder {
     private String name;
@@ -57,5 +64,59 @@ public class CardBuilder {
     public CardBuilder setEvent(Event event) {
         this.event = event;
         return this;
+    }
+
+
+    public static List<Card> buildCardsFromFile(File file {
+        List<Card> cards = new ArrayList<>();
+
+        try (var reader = new BufferedReader(new FileReader(file))) {
+            String l;
+            while((l = reader.readLine()) != null) {
+                var line = l.split(",");
+                if (line.length != 6) {
+                    System.err.println("Could not parse line, skipping: " + Arrays.toString(line));
+                    continue;
+                }
+                var cardBuilder = new CardBuilder();
+                cardBuilder.setName(line[0]);
+                cardBuilder.setType(switch (Integer.getInteger(line[1])) {
+                    case 1 -> Type.PRIMARY_INDUSTRY;
+                    case 2 -> Type.SECONDARY_INDUSTRY;
+                    case 3 -> Type.RESTAURANTS;
+                    case 4 -> Type.MAYOR_ESTABLISHMENT;
+                    case 5 -> Type.LANDMARK;
+                    default -> throw new IllegalStateException("Unexpected value: " + line[2]);
+                });
+                cardBuilder.setCardClass(switch (line[2]) {
+                    case "feld" -> CardClass.WHEAT;
+                    case "tier" -> CardClass.COW;
+                    case "laden" -> CardClass.SUITCASE;
+
+                    default -> throw new IllegalStateException("Unexpected value: " + line[2]);
+                });
+                cardBuilder.setCost(Integer.getInteger(line[3]));
+                cardBuilder.setFront(new Image(line[4]));
+                cardBuilder.setBack(new Image(line[5]));
+                cardBuilder.setEvent(getEvent(line[0]));
+                cards.add(cardBuilder.build());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SlickException e) {
+            throw new RuntimeException(e);
+        }
+        return cards;
+    }
+
+    private static Event getEvent(String cardName) {
+        return new Event("CardEvent_" + cardName) {
+            @Override
+            protected boolean performAction(GameContainer gc, StateBasedGame sb, int delta) {
+                System.out.println("Card Event " + cardName + " triggered.");
+                return false;
+            }
+        };
+
     }
 }
